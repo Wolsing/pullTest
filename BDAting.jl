49c7069c04b034a87da18266970a6fc78102457e
testWLaus <69089801+testWLaus@users.noreply.github.com>
2020-08-01 21:34:06 +0200

    first add

---
+++ BDAting.jl
@@ -0,0 +1,29 @@
+
+using Revise
+using Distributions
+using Turing
+using Plots
+
+
+#sum([log(n) / factorial(big(n)) for n = 1:num])
+num = 50
+listen = zeros(num)
+summen = 0
+for i = 1:num
+    global summen +=  log(i)/factorial(big(i))
+    listen[i] = summen
+end
+listen
+
+scatter(1:num,listen)
+
+listen
+
+#using Cairo
+#using Fontconfig
+#draw(PNG("myplot2.png", 7inch, 7inch), p1)
+
+
+
+
+
